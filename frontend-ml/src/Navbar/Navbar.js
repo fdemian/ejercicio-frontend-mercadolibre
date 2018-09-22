de8166e1d0c682d9de3769b92c64c7c3e22d7bbd
Federico Caminiti <fdemian@MacBook-Pro-de-Federico.local>
2018-09-22 17:21:28 -0300

    Aesthetic changes to the homepage and SearchBar.

--- frontend-ml/src/Navbar/Navbar.js
+++ frontend-ml/src/Navbar/Navbar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import logoML from '../assets/Logo_ML.png';
+import logoSearch from '../assets/ic_Search.png';
+import './Navbar.css';
 
 const Navbar = () => {
     return(
-    <div>
-      <h1>ML Home</h1>
+    <div className="NavbarContainer">
+     <div>
+      <img src={logoML} alt="Mercado Libre" className="LogoML" />
+      <input type="text" placeholder="Nunca dejes de buscar" className="SearchInput"/>
+      <button type="button" className="SearchButton">
+        <img src={logoSearch} alt="Busqueda" className="SearchLogo" />
+      </button>
+     </div>
     </div>
     );
 }
--- frontend-ml/src/Routes/Routes.js
+++ frontend-ml/src/Routes/Routes.js
@@ -2,18 +2,14 @@ import App from '../App/App';
 import Home from '../Home/Home';
 import ProductDetails from '../ProductDetails/ProductDetails';
 import SearchResults from '../SearchResults/SearchResults';
-                             
+
 const ApplicationRoutes = {
    App: App,
    Routes: [
     {path: "/", exact: true, component: Home },
     {path: "/ProductDetails", exact: true, component: ProductDetails},
     {path: "/SearchResults", exact: true, component: SearchResults}
-    /*{path: "/auth", exact: true, component: Authentication, private: false},
-    {path: "/login", exact: true, component: Login, private: false},
-    {path: "/register", exact: true, component: Register, private: false},
-    {path: '/stories/:id/:name', exact: true, component: Story, private: false}
-    {component: NotFound }*/
+    /*{component: NotFound }*/
    ]
 };
 
